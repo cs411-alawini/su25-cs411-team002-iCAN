658ea9243954940fc74a68a9341a8547906a37cd
Tanjie McMeans <mcmeans2@illinois.edu>
2025-07-27 12:22:29 -0500

    setup set query for /pokedex

---
+++ backend/run.py
@@ -0,0 +1,8 @@
+from app import create_app
+
+# Create the Flask app instance using the factory
+app = create_app()
+
+# Runs when 'python run.py' is ran in the terminal
+if __name__ == '__main__':
+    app.run(debug=True) # Start the development server 
